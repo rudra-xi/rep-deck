824947464bd0061b398c1767a71a86f8ed3e5e10
rudra <goutam.prasad.2707@gmail.com>
2026-08-20 23:48:46 +0530

    update : google img protocol

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
 	/* config options here */
 	reactCompiler: true,
 	allowedDevOrigins: ["localhost:3000", "192.168.1.8:3000", "192.168.1.8"],
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "lh3.googleusercontent.com",
+				port: "",
+				pathname: "/**",
+			},
+		],
+	},
 };
 
 export default nextConfig;
