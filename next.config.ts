6f90cca25a6b660c5810dc7b1a0f1fd487c7ba58
rudra <goutam.prasad.2707@gmail.com>
2026-08-24 00:06:23 +0530

    chore : dicebear protocol

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,23 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	reactCompiler: true,
-	allowedDevOrigins: ["localhost:3000", "192.168.1.8:3000", "192.168.1.8"],
+	allowedDevOrigins: [
+		"localhost:3000",
+		"192.168.1.8:3000",
+		"192.168.1.8",
+		"reword-frigidly-action.ngrok-free.dev",
+	],
 	images: {
 		remotePatterns: [
 			{
 				protocol: "https",
 				hostname: "lh3.googleusercontent.com",
+				port: "",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "api.dicebear.com",
 				port: "",
 				pathname: "/**",
 			},
