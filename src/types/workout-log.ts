27f35b191a9ee9d5ed9b414b168859b9f7827772
rudra <goutam.prasad.2707@gmail.com>
2026-08-26 19:07:05 +0530

    new : types for LoggetSet

---
+++ src/types/workout-log.ts
@@ -0,0 +1,23 @@
+// src/types/workout.ts
+export type LoggedSet = {
+  id: string;
+  exerciseName: string;
+  weight: string;
+  reps: string;
+  rpe: string;
+};
+
+// You can also add other workout-related types here
+export type Exercise = {
+  id: string;
+  name: string;
+  type: "Primary" | "Secondary" | "Accessory";
+  target: string;
+  lastSession: string;
+};
+
+export type Day = {
+  id: number;
+  label: string;
+  exercises?: Exercise[];
+};
