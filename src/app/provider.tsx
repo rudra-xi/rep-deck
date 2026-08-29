8362fd19fbdbba2bc319c54d01efb24cf4635b56
rudra <goutam.prasad.2707@gmail.com>
2026-08-29 17:29:36 +0530

    chore : toaster added

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -3,15 +3,17 @@
 import { usePathname } from "next/navigation";
 import type { ReactNode } from "react";
 import { Footer, Navigation } from "@/common";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function LayoutProvider({ children }: { children: ReactNode }) {
 	const pathname = usePathname();
-	const hideOnRoutes = ["/", "/login", "/signup"];
+	const hideOnRoutes = "/";
 	const show = !hideOnRoutes.includes(pathname);
 	return (
 		<>
 			{show && <Navigation />}
 			{children}
+			<Toaster position="top-right" />
 			{show && <Footer />}
 		</>
 	);
