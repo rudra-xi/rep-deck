aedd673948050fc5292b04f81288768e0ff79cab
rudra <goutam.prasad.2707@gmail.com>
2026-09-06 22:38:35 +0530

    chore : ui fix

--- src/components/@common/back-button.tsx
+++ src/components/@common/back-button.tsx
@@ -4,18 +4,22 @@ import { ArrowLeftIcon } from "@phosphor-icons/react";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 
-export const BackButton = () => {
+interface BackButtonProps {
+	text?: string;
+}
+
+export const BackButton = ({ text }: BackButtonProps) => {
 	const router = useRouter();
 
 	return (
 		<Button
 			variant="outline"
-			size="icon"
+			size="sm"
 			onClick={() => router.back()}
-			className=" square size-10 border-foreground/20 hover:bg-muted"
 			aria-label="Go back"
 		>
-			<ArrowLeftIcon className="size-5" />
+			<ArrowLeftIcon />
+			<span className="capitalize text-xs">{text}</span>
 		</Button>
 	);
 };
