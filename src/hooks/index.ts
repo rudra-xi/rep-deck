8565dae729695304d591e47503d1d210fe75fab7
rudra <goutam.prasad.2707@gmail.com>
2026-09-07 22:55:06 +0530

    new : hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,9 +3,17 @@ import { useDashboardData } from "./dashboard-hook/use-dashboard-data";
 import { useKpiCards } from "./dashboard-hook/use-kpi-cards";
 import { useLastWorkout } from "./dashboard-hook/use-last-workout";
 import { useStrengthChart } from "./dashboard-hook/use-strength-chart";
+
 import { useDialog } from "./plans-hook/use-dialog";
 import { useDialogForm } from "./plans-hook/use-dialog-form";
 import { useFormField } from "./plans-hook/use-form-field";
+
+import { useBodyMetrics } from "./progress-hook/use-body-metrics";
+import { useLiftDetails } from "./progress-hook/use-lift-details";
+import { useSessionHistory } from "./progress-hook/use-session-history";
+import { useStrengthOverview } from "./progress-hook/use-strength-overview";
+import { useTrainingFrequency } from "./progress-hook/use-training-frequency";
+
 import { useExerciseInputs } from "./workout-log-hook/use-exercise-inputs";
 import { useExercisePerformance } from "./workout-log-hook/use-exercise-performance";
 import { useRangedInput } from "./workout-log-hook/use-ranged-input";
@@ -26,4 +34,9 @@ export {
 	useDashboardData,
 	useKpiCards,
 	useLastWorkout,
+	useBodyMetrics,
+	useLiftDetails,
+	useSessionHistory,
+	useStrengthOverview,
+	useTrainingFrequency,
 };
