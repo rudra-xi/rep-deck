617552905b78f30fe12edaa299d122f2161b295c
rudra <goutam.prasad.2707@gmail.com>
2026-09-12 22:51:32 +0530

    new : 6 themes

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -4,17 +4,31 @@ import { usePathname } from "next/navigation";
 import type { ReactNode } from "react";
 import { Footer, Navigation } from "@/common";
 import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "next-themes";
 
 export default function LayoutProvider({ children }: { children: ReactNode }) {
 	const pathname = usePathname();
 	const hideOnRoutes = "/";
 	const show = !hideOnRoutes.includes(pathname);
 	return (
-		<>
+		<ThemeProvider
+			attribute="class"
+			defaultTheme="enterprise"
+			enableSystem={false}
+			themes={[
+				"enterprise",
+				"qraft",
+				"rosepine",
+				"zen",
+				"opcl",
+				"barmell",
+			]}
+			disableTransitionOnChange
+		>
 			{show && <Navigation />}
 			{children}
 			<Toaster position="top-right" />
 			{show && <Footer />}
-		</>
+		</ThemeProvider>
 	);
 }
