ac651e5990d6830d2a986265a065c2970dd5ccb9
rudra <goutam.prasad.2707@gmail.com>
2026-09-23 02:23:04 +0530

    update : idx

--- src/components/@plans/@plan-dialogs/index.ts
+++ src/components/@plans/@plan-dialogs/index.ts
@@ -4,7 +4,9 @@ import { CreatePlanDialog } from "./create-plan-dialog";
 import { DeleteDayDialog } from "./delete-day-dialog";
 import { DeleteExerciseDialog } from "./delete-exercise-dialog";
 import { DeletePlanDialog } from "./delete-plan-dialog";
+import { DuplicateDayDialog } from "./duplicate-day-dialog";
 import { DuplicatePlanDialog } from "./duplicate-plan-dialog";
+import { EditDayDialog } from "./edit-day-dialog";
 import { EditExerciseDialog } from "./edit-exercise-dialog";
 
 export {
@@ -12,6 +14,8 @@ export {
 	DeletePlanDialog,
 	CreateDayDialog,
 	DeleteDayDialog,
+	EditDayDialog,
+	DuplicateDayDialog,
 	EditExerciseDialog,
 	DeleteExerciseDialog,
 	CreateExerciseDialog,
